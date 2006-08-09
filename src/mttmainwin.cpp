f94c9462a4756c3088b45fcd599708a468d14c9b
Theofilos Intzoglou <int.teo@gmail.com>
2006-08-09 18:38:11 +0000

    * Add genre list

--- src/mttmainwin.cpp
+++ src/mttmainwin.cpp
@@ -47,10 +47,28 @@ mttMainWin::mttMainWin(QWidget* parent, const char* name, WFlags fl)
 : MainForm(parent,name,fl)
 {
     int i;
+    static const char* genres[] = { "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk",
+                                    "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies",
+                                    "Other", "Pop", "R&B", "Rap", "Reggae", "Rock", "Techno",
+                                    "Industrial", "Alternative", "Ska", "Death Metal", "Pranks",
+                                    "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal",
+                                    "Jazz+Funk", "Fusion", "Trance", "Classical", "Instrumental",
+                                    "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise",
+                                    "AlternRock", "Bass", "Soul", "Punk", "Space", "Meditative",
+                                    "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic",
+                                    "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk",
+                                    "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult",
+                                    "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle",
+                                    "Native American", "Cabaret", "New Wave", "Psychadelic",
+                                    "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk",
+                                    "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll",
+                                    "Hard Rock", 0 };
 
     LZ = true; // Leading Zeros
     LZ1 = "0";
     LZ2 = "";
+
+    GenGenreCB->insertStrList( genres );
 
     d.setFilter( QDir::Files | QDir::Readable );
     d.setNameFilter( QString( "*.mp3;*.MP3;*.ogg;*.OGG;*.flac;*.FLAC" ) );
