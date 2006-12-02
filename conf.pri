a003d3fa59f8cdd2870eaace47ed6b6fc8920334
Theofilos Intzoglou <int.teo@gmail.com>
2006-12-02 00:42:00 +0000

    * Added new build system using a configure script generated by qconf

---
+++ conf.pri
@@ -0,0 +1,5 @@
+# qconf
+QT_PATH_PLUGINS = /usr/qt/3/plugins
+
+target.path = /usr/local/bin
+INSTALLS += target
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Check for taglib version > 1.4
+echo -n "Checking for taglib v1.4 or higher..."
 taglib_version="`taglib-config --version`"
 if [[ "$taglib_version" < "1.4" ]];then
 	echo -e "Taglib v1.4 or greater not found!\nPlease install taglib and try again later."
 	exit 1;
 fi
+echo "ok"
 
 # Read prefix for installation
-echo -en "Where do you want the files to be installed [/usr/local/]? "
-read -r ppath
-if [[ "$ppath" == "" ]];then
-	INSTALL_ROOT="/usr/local/"
-else
-	INSTALL_ROOT="${ppath%%/}/"
-fi
-echo -n "$INSTALL_ROOT" > PREFIX
+#echo -en "Where do you want the files to be installed [/usr/local/]? "
+#read -r ppath
+#if [[ "$ppath" == "" ]];then
+#	INSTALL_ROOT="/usr/local/"
+#else
+#	INSTALL_ROOT="${ppath%%/}/"
+#fi
+#echo -n "$INSTALL_ROOT" > PREFIX
 
 # Find out Media Tag Tools version
 echo -e "#define VERSION \"`cat VERSION`\"" > src/config.h
@@ -33,5 +35,6 @@ fi
 echo -e "\nConfig options"
 echo -e "--------------"
 echo -e "Taglib version	= $taglib_version"
-echo -e "Prefix		= $INSTALL_ROOT\n"
+#echo -e "Prefix		= $INSTALL_ROOT\n"
+echo -e "Prefix		= $PREFIX\n"
 echo -e "MTT version	= $VERSION"
