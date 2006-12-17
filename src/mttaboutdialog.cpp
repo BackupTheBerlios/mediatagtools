2e6f4e74607e1152723ed927bda219a276880fb9
Theofilos Intzoglou <int.teo@gmail.com>
2006-12-17 15:38:30 +0000

    * About dialog finished

--- src/mttaboutdialog.cpp
+++ src/mttaboutdialog.cpp
@@ -12,7 +12,7 @@
 
 #include <qlabel.h>
 #include <qpainter.h>
-#include <qpicture.h>
+#include <qimage.h>
 
 #include "mttaboutdialog.h"
 #include "config.h"
@@ -27,12 +27,6 @@ mttAboutDialog::mttAboutDialog(QWidget* parent, const char* name, bool modal, WF
 #ifndef RELEASE
     ProgNameLabel->setText( QString( "<p align=\"center\"><b><h3>Media Tag Tools v." ) + VERSION + "-" + RV_SNAPSHOT_VERSION + "</h3></b></p>" );
 #endif
-    QPicture  pic;
-    pic.load( PREFIX + QString( "/share/mediatagtools/icons/logo.svg" ), "svg" );     // load picture
-    QPainter  p;
-    p.begin( LogoFrame );          // paint in myWidget
-    p.drawPicture( pic );          // draw the picture
-    p.end();                       // painting done
 }
 
 mttAboutDialog::~mttAboutDialog()
